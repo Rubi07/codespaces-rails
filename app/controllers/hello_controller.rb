d3569f1cb2db15ef942df105ca18d8f22fb8ede7
Rubi07 <118752991+Rubi07@users.noreply.github.com>
2023-01-21 19:43:42 +0000

    pages

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,4 +1,4 @@
-class HelloController < ApplicationController
+class HelolController < ApplicationController
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  root "hello#index"
+  get 'pages/about_us'
+  get 'pages/contact_us'
+  get 'pages/privacy_policy'
+  get 'pages/terms_and_conditions'
+ root "home#index"
 end
+  
